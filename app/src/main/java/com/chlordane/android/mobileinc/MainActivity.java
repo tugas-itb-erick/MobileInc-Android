170a998822d993b06336f226ee190e25629bc210
Roland Hartanto <13515107@std.stei.itb.ac.id>
2017-09-26 17:34:58 +0700

    setting activity added

--- app/src/main/java/com/chlordane/android/mobileinc/MainActivity.java
+++ app/src/main/java/com/chlordane/android/mobileinc/MainActivity.java
@@ -101,6 +101,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settingIntent = new Intent(getApplicationContext(),SettingsActivity.class);
+            startActivityForResult(settingIntent,TEXT_REQUEST);
             return true;
         }
 
