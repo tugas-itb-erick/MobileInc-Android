2a9e0ba7f480b3f03617d953afb3c5615e79702b
wijayaerick <wijaya.erick52@gmail.com>
2017-09-30 21:30:40 +0700

    theme setting

--- app/src/main/java/com/chlordane/android/mobileinc/MainActivity.java
+++ app/src/main/java/com/chlordane/android/mobileinc/MainActivity.java
@@ -2,8 +2,10 @@ package com.chlordane.android.mobileinc;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
@@ -151,6 +153,14 @@ public class MainActivity extends AppCompatActivity implements
                 .build();
 
         mAuth = FirebaseAuth.getInstance();
+
+        // Just to Log SharefPreferences Key-Values
+        Map<String,?> keys = PreferenceManager.getDefaultSharedPreferences(this).getAll();
+        for(Map.Entry<String,?> entry : keys.entrySet()){
+            Log.d("SharedPrefs",entry.getKey() + ": " +
+                    entry.getValue().toString());
+        }
+
     }
 
     @Override
