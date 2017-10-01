b8d4319eb96ee43151d9bdd00ab71eea9a1f7602
Roland Hartanto <13515107@std.stei.itb.ac.id>
2017-10-01 18:27:53 +0700

    load qr code from url done

---
+++ app/src/main/java/com/chlordane/android/mobileinc/YourQRCodeActivity.java
@@ -0,0 +1,31 @@
+package com.chlordane.android.mobileinc;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class YourQRCodeActivity extends AppCompatActivity {
+
+    private SharedPreferences mPreferences;
+    private static final String mSharedPrefFile = "com.chlordane.android.mobileinc";
+    private ImageView imageView;
+    private final String PROMO_KEY = "promo_key";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_qrcode);
+
+        mPreferences = getSharedPreferences(mSharedPrefFile, Context.MODE_PRIVATE);
+        imageView = (ImageView) findViewById(R.id.qrCodeImage);
+
+        try {
+            Picasso.with(getApplicationContext()).load("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example").into(imageView);
+        }catch (Exception e){
+
+        }
+    }
+}
